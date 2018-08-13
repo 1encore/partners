04040ccf1ed95213cdc62b7fd18170552388b837
1encore <hg1zadr@gmail.com>
2018-08-13 17:48:11 +0600

    add: -editing fields -adding fields -deleting fields

--- pages/profile.php
+++ pages/profile.php
@@ -5,55 +5,70 @@ if($connection->query("SET NAMES utf8")){
     $query_com = $connection->query("SELECT * FROM companies WHERE id = $com_id");
     if($row_com = $query_com->fetch_object()){
 ?>
-
-<!-- Modal -->
-<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered" role="document">
+<!-- Modal Add-->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
     <div class="modal-content">
       <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLongTitle">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
+        <h5 class="modal-title" id="exampleModalLabel">Новое поле</h5>
       </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
+      <form class="" action="?act=addField" method="post" enctype="multipart/form-data">
+        <div class="modal-body">
+          <input type="text" name="userfile" required placeholder="Фото" onmouseover="(this.type='file')" class="form-control">
+          <select name="type" required>
+            <option disabled selected>Тип</option>
+          <?php
+            $query_add_type = $connection->query("SELECT * FROM type");
+            while($row_add_type = $query_add_type->fetch_object()){
+          ?>
+            <option value="<?php echo $row_add_type->id ?>"><?php echo $row_add_type->name; ?></option>
+          <?php
+            }
+          ?>
+          </select>
+          <select name="coverage" required>
+            <option disabled selected>Покрытие</option>
+          <?php
+            $query_add_type = $connection->query("SELECT * FROM coverage");
+            while($row_add_type = $query_add_type->fetch_object()){
+          ?>
+            <option value="<?php echo $row_add_type->id ?>"><?php echo $row_add_type->name; ?></option>
+          <?php
+            }
+          ?>
+        </select>
+        </div>
+        <div class="modal-footer">
+          <button type="button" class="btn btn-secondary" data-dismiss="modal">Закрыть</button>
+          <button type="submit" class="btn btn-primary">Добавить</button>
+        </div>
+      </form>
     </div>
   </div>
 </div>
-
 <!-- Cart -->
 <section class="cart">
     <div class="container">
         <div class="row">
-
             <div class="col-md-12 cart-total-items">
                 <h5 class="subheading">Кол-во полей: <?php echo $row_com->field_count; ?></h5>
             </div>
+            <div class="col-md-8">
+            <!-- Getting each field -->
             <?php
-            $query_field = $connection->query("SELECT * FROM fields WHERE company_id = $row_com->id");
+            $query_field = $connection->query("SELECT * FROM fields WHERE company_id = $row_com->id AND active = 1");
             $num = 0;
             while($row_field = $query_field->fetch_object()){
-              if($row_field->active){
               $num++;
             ?>
-            <div class="col-md-8">
-
                 <div class="cart-item">
                     <div class="item-image-container">
                         <?php
-                        $query_pic = $connection->query("SELECT * FROM pictures WHERE field_id = $row_field->id");
+                        $query_pic = $connection->query("SELECT * FROM pictures WHERE field_id = $row_field->id AND active = 1");
                         if($row_pic = $query_pic->fetch_object()){
-                          if($row_pic->active){
                         ?>
                           <a href="#"><img src="db/img/<?php echo $row_pic->url; ?>" class="img-responsive" alt="#"></a>
                         <?php
-                          }
                         }
                         ?>
                     </div>
@@ -69,15 +84,73 @@ if($connection->query("SET NAMES utf8")){
                           <span>,</span>
                           <?php echo $row_type->cov; ?>
                         <?php } ?>
-                        <a href="#" class="item-action move-to-wishlist"><i class="ion-android-list"></i>Редактировать</a>
-                        <a href="#" class="item-action remove-from-cart"><i class="ion-ios-trash-outline"></i>Удалить</a>
+                        <a href="#" class="item-action move-to-wishlist" data-toggle="modal" data-target="#<?php echo $row_field->id ?>"><i class="ion-android-list"></i>Редактировать</a>
+                        <a href="?act=delField&id=<?php echo $row_field->id ?>" class="item-action remove-from-cart"><i class="ion-ios-trash-outline"></i>Удалить</a>
                     </div>
                 </div>
-            </div>
+                <!-- Modal edit-->
+                <div class="modal fade" id="<?php echo $row_field->id ?>" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                  <div class="modal-dialog" role="document">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Новое поле</h5>
+                      </div>
+                      <form class="" action="?act=editField&id=<?php echo $row_field->id; ?>" method="post" enctype="multipart/form-data">
+                        <div class="modal-body">
+                          <input type="text" name="userfile" placeholder="Заменить текущее фото" onmouseover="(this.type='file')" class="form-control">
+                          <select name="type" required>
+                            <option disabled>Тип</option>
+                          <?php
+                            $query_add_type = $connection->query("SELECT * FROM type");
+                            while($row_add_type = $query_add_type->fetch_object()){
+                              if($row_add_type->id == $row_field->type_id){
+                          ?>
+                            <option selected value="<?php echo $row_add_type->id ?>"><?php echo $row_add_type->name; ?></option>
+                          <?php
+                              }else{
+                          ?>
+                              <option value="<?php echo $row_add_type->id ?>"><?php echo $row_add_type->name; ?></option>
+                          <?php
+                              }
+                            }
+                          ?>
+                          </select>
+                          <select name="coverage" required>
+                            <option disabled>Тип</option>
+                          <?php
+                            $query_add_type = $connection->query("SELECT * FROM coverage");
+                            while($row_add_type = $query_add_type->fetch_object()){
+                              if($row_add_type->id == $row_field->coverage_id){
+                          ?>
+                            <option selected value="<?php echo $row_add_type->id ?>"><?php echo $row_add_type->name; ?></option>
+                          <?php
+                              }else{
+                          ?>
+                              <option value="<?php echo $row_add_type->id ?>"><?php echo $row_add_type->name; ?></option>
+                          <?php
+                              }
+                            }
+                          ?>
+                          </select>
+                        </div>
+                        <div class="modal-footer">
+                          <button type="button" class="btn btn-secondary" data-dismiss="modal">Закрыть</button>
+                          <button type="submit" class="btn btn-primary">Изменить</button>
+                        </div>
+                      </form>
+                    </div>
+                  </div>
+                </div>
             <?php
-              }
             }
             ?>
+            <!-- Add field -->
+              <div class="cart-item">
+                <div class="item-image-container cbp-item">
+                      <a href="#" data-toggle="modal" data-target="#exampleModal"><img src="img/assets/new.jpg" class="img-responsive" alt="#"></a>
+                </div>
+              </div>
+            </div>
 
             <div class="col-md-4">
                 <div class="cart_totals">
@@ -109,7 +182,6 @@ if($connection->query("SET NAMES utf8")){
                     </table>
                 </div>
                 <a href="#" class="btn btn-primary btn-md btn-appear btn-cart-checkout"><span>Редактировать<i class="ion-ios-arrow-forward"></i></span></a>
-                <a href="#" class="btn btn-primary btn-md btn-appear btn-cart-checkout" data-toggle="modal" data-target="#exampleModalCenter"><span>Добавить поле<i class="ion-ios-arrow-forward"></i></span></a>
             </div>
 
         </div>
--- templates/engine.php
+++ templates/engine.php
@@ -27,11 +27,81 @@ if(isset($_GET['act'])){
     $city = $_POST['city'];
     $dis = $_POST['district'];
 
-    if($connection->query("INSERT INTO companies VALUES(NULL, \"$name\", \"$email\", \"$pwd\", $city, $dis, \"$addr\", 1, 1, \"$phone\", 0, \"$descr\", 1)")){
+    if($connection->query("INSERT INTO companies VALUES(NULL, \"$name\", \"$email\", \"$pwd\", $city, $dis, \"$addr\", 1, 1, \"$phone\", 1, \"$descr\", 1)")){
       header('Location:index.php?page=login&reg=1');
     }else{
       header('Location:index.php?page=registration&error=1');
     }
+  }else if($_GET['act'] == "addField" && isset($_SESSION['id'])){
+
+    $type = $_POST['type'];
+    $cov = $_POST['coverage'];
+    $temp_file = $_FILES['avatar'];
+    $salt = "dratuti";
+    $id = $_SESSION['id'];
+    $query_add_field=$connection->query("SELECT id FROM pictures WHERE id = (SELECT MAX(id) FROM pictures)");
+    if($row_add_field=$query_add_field->fetch_object()){
+      $img_max = $row_add_field->id;
+      $img_max++;
+	    $file_name = sha1($img_max.$salt).".jpg";
+	    move_uploaded_file($_FILES['userfile']['tmp_name'],"db/img/$file_name");
+      $connection->query("INSERT INTO fields VALUES(NULL,$type,$cov,$id,1)");
+      $query_add_img = $connection->query("SELECT id FROM fields WHERE id = (SELECT MAX(id) FROM fields)");
+      if($row_add_img = $query_add_img->fetch_object()){
+        $id_max = $row_add_img->id;
+        $connection->query("INSERT INTO pictures VALUES(NULL,\"$file_name\",$id_max,1)");
+      }
+    }
+
+    //updating field_count in companies
+    $id = $_SESSION['id'];
+    $connection->query("UPDATE fields SET active = 0 WHERE id = $id_del");
+    $query_del_field = $connection->query("SELECT * FROM companies WHERE id = $id");
+    if($row_del_field = $query_del_field->fetch_object()){
+      $field_count = $row_del_field->field_count;
+      $field_count++;
+      $connection->query("UPDATE companies SET field_count = $field_count WHERE id = $id");
+    }
+
+    header('Location:index.php?page=profile');
+
+  }else if($_GET['act'] == 'editField' && isset($_SESSION['id'])){
+
+    $id_edit = $_GET['id'];
+    $type = $_POST['type'];
+    $cov = $_POST['coverage'];
+    $temp_file = $_FILES['avatar'];
+
+    $connection->query("UPDATE fields SET type_id = $type, coverage_id = $cov WHERE id = $id_edit");
+
+    if(file_exists($_FILES['userfile']['tmp_name']) || is_uploaded_file($_FILES['userfile']['tmp_name'])) {
+      $connection->query("UPDATE pictures SET active = 0 WHERE field_id = $id_edit");
+      $salt = "dratuti";
+      $query_add_field=$connection->query("SELECT id FROM pictures WHERE id = (SELECT MAX(id) FROM pictures)");
+      if($row_add_field=$query_add_field->fetch_object()){
+        $img_max = $row_add_field->id;
+        $img_max++;
+        $file_name = sha1($img_max.$salt).".jpg";
+        move_uploaded_file($_FILES['userfile']['tmp_name'],"db/img/$file_name");
+        $id_max = $row_add_img->id;
+        $connection->query("INSERT INTO pictures VALUES(NULL,\"$file_name\",$id_edit,1)");
+      }
+    }
+    header('Location:index.php?page=profile');
+
+  }else if($_GET['act'] == 'delField'){
+
+    $id = $_SESSION['id'];
+    $id_del = $_GET['id'];
+    $connection->query("UPDATE fields SET active = 0 WHERE id = $id_del");
+    $query_del_field = $connection->query("SELECT * FROM companies WHERE id = $id");
+    if($row_del_field = $query_del_field->fetch_object()){
+      $field_count = $row_del_field->field_count;
+      $field_count--;
+      $connection->query("UPDATE companies SET field_count = $field_count WHERE id = $id");
+    }
+    header('Location:index.php?page=profile');
+
   }else if($_GET['act'] == 'logout'){
     unset($_SESSION['id']);
     header('Location:index.php?page=login');
@@ -43,7 +113,7 @@ if(isset($_GET['page'])){
     $page = 'registration';
   }else if($_GET['page'] == 'login'){
     $page = 'login';
-  }else if($_GET['page'] == 'profile'){
+  }else if($_GET['page'] == 'profile' && isset($_SESSION['id'])){
     $page = 'profile';
   }else{
     $page = '404';
